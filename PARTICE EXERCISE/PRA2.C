945e0c6aa25309c6898d5569adc155d8152152f2
192511170simats-spec <192511170.simats@saveetha.com>
2026-08-01 13:38:58 +0530

    Create PRA2.C

---
+++ PARTICE EXERCISE/PRA2.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid = fork();
+
+    if(pid == 0)
+    {
+        printf("Child Process Running...\n");
+        printf("Child Process Terminating.\n");
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+        printf("Parent Process: Child Completed.\n");
+    }
+
+    return 0;
+}
